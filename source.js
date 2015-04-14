0a33533807f5c16379ff9057c34465e839cc2587
Wliu <50Wliu@users.noreply.github.com>
2015-04-14 11:30:39 -0400

    Reset arrays

--- source.js
+++ source.js
@@ -54,6 +54,9 @@ function startTimer(){  //Called from HTML
 		prePosYVal = 250;
 		$('#slider').text(0);
 		document.getElementById("startTimer").innerHTML = "Stop";
+		posArray = [];
+		vecArray = [];
+		accArray = [];
 		sliderPosition = 0;
 		speed = 0;
 		moving = 0;
