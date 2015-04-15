cdfa56402b24b695e9753a9d23f910675d8b5841
Wliu <Wliu1402@gmail.com>
2015-04-14 20:43:03 -0400

    Rename a whole bunch of variables == --> ===

--- source.js
+++ source.js
@@ -1,13 +1,15 @@
+"use strict";
+
 var canvas = document.getElementById('graphCanvas');
-var timeCounter;
-var prePosYVal;
-var posArray = [];
-var velArray = [];
-var accArray = [];
 var timer;
-var moving = 0;
-var speed = 0;
-var acc = 0.2;
+var time;
+var previousYValue;
+var positionArray = [];
+var velocityArray = [];
+var accelerationArray = [];
+var inertia = 0;  //Not entirely accurate but it gets the meaning across
+var velocity = 0;
+var acceleration = 0.2;
 var sliderPosition = 0;
 var paused = false;
 var stopped = false;
@@ -16,26 +18,26 @@ init();
 function init(){
 	createCanvas();
 	$("#toggleTimer").hide();
-	document.addEventListener('keydown', function(event) {
-	if(event.keyCode == 37 || event.keyCode == 40 || event.keyCode == 65 || event.keyCode == 83) {  //Left arrow, down arrow, A key, S key
-		event.preventDefault();
-		speed = 0;
-		speed -= acc;
-	}
-	else if(event.keyCode == 39 || event.keyCode == 38 || event.kaceyCode == 68 || event.keyCode == 87) {  //Right arrow, up arrow, D key, W key
-		event.preventDefault();
-		speed = 0;
-		speed += acc;
-	}
-	else if(event.keyCode == 32){  //Spacebar
-		event.preventDefault();
-		toggleTimer();
-	}
+	document.addEventListener('keydown', function(event){
+		if(event.keyCode === 37 || event.keyCode === 40 || event.keyCode === 65 || event.keyCode === 83){  //Left arrow, down arrow, A key, S key
+			event.preventDefault();
+			velocity = 0;
+			velocity -= acceleration;
+		}
+		else if(event.keyCode === 38 || event.keyCode === 39 || event.keyCode === 68 || event.keyCode === 87){  //Up arrow, right arrow, D key, W key
+			event.preventDefault();
+			velocity = 0;
+			velocity += acceleration;
+		}
+		else if(event.keyCode === 32){  //Spacebar
+			event.preventDefault();
+			toggleTimer();
+		}
 	});
 
 	document.addEventListener('keyup', function(event){
-		if(event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40 || event.keyCode == 65 || event.keyCode == 68 || event.keyCode == 83 || event.keyCode == 87){
-			speed = 0;
+		if(event.keyCode === 37 || event.keyCode === 38 || event.keyCode === 39 || event.keyCode === 65 || event.keyCode === 68 || event.keyCode === 83 || event.keyCode === 87){
+			velocity = 0;
 		}
 	});
 }
@@ -50,18 +52,19 @@ function createCanvas(){
 	context.stroke();
 }
 
-function startTimer(){  //Called from HTML
-	if(stopped || document.getElementById("startTimer").innerHTML == "Click to start"){
-		timeCounter = 0;
-		prePosYVal = 250;
+//Called from the HTML file
+function startTimer(){  //jshint ignore:line
+	if(stopped || document.getElementById("startTimer").innerHTML === "Click to start"){
+		time = 0;
+		previousYValue = 250;
 		$('#slider').text(0);
 		document.getElementById("startTimer").innerHTML = "Stop";
-		posArray.length = 0;
-		velArray.length = 0;
-		accArray.length = 0;
+		positionArray.length = 0;
+		velocityArray.length = 0;
+		accelerationArray.length = 0;
 		sliderPosition = 0;
-		speed = 0;
-		moving = 0;
+		velocity = 0;
+		inertia = 0;
 		createCanvas();
 		$("#toggleTimer").show();
 		if(timer !== null){
@@ -72,8 +75,8 @@ function startTimer(){  //Called from HTML
 	}
 	else{
 		document.getElementById("startTimer").innerHTML = "Restart";
-		drawVel();
-		drawAcc();
+		drawVelocity();
+		drawAcceleration();
 		window.clearInterval(timer);
 		$("#toggleTimer").hide();
 		stopped = true;
@@ -94,28 +97,28 @@ function toggleTimer(){
 }
 
 function updateSliderPosition(){
-	moving += speed;
-	$('#slider').text(parseInt(document.getElementById('slider').innerHTML)+Math.round(moving));  //Update the slider's text
+	inertia += velocity;
+	$('#slider').text(parseInt(document.getElementById('slider').innerHTML)+Math.round(inertia));  //Update the slider's text
 
-	var position = -(parseFloat(document.getElementById('slider').innerHTML)+moving)+250;  //Invert the position so that it displays properly then add 250 because that's the center of the graph
+	var position = -(parseFloat(document.getElementById('slider').innerHTML)+inertia)+250;  //Invert the position so that it displays properly then add 250 because that's the center of the graph
 	if(position < 0){
 		position = 0;
-		speed = 0;
-		moving = 0;
+		velocity = 0;
+		inertia = 0;
 		$('#slider').text(250);
 	}
 	else if(position > 500){
 		position = 500;
-		speed = 0;
-		moving = 0;
+		velocity = 0;
+		inertia = 0;
 		$('#slider').text(-250);  //Update the slider's text
 	}
 
-	drawPos(position);
-	timeCounter++;
-	if(timeCounter>1000){
-		drawVel();
-		drawAcc();
+	drawPosition(position);
+	time++;
+	if(time>1000){
+		drawVelocity();
+		drawAcceleration();
 		window.clearInterval(timer);
 		$("#toggleTimer").hide();
 		document.getElementById("toggleTimer").innerHTML = "Pause";
@@ -125,48 +128,48 @@ function updateSliderPosition(){
 	}
 }
 
-function drawPos(graphPosition){
+function drawPosition(graphPosition){
 	var context = canvas.getContext('2d');
 	context.strokeStyle="#000000";
 	context.beginPath();
-	draw(context, timeCounter-1, prePosYVal, timeCounter, graphPosition);
-	if(posArray[timeCounter-1] === null){
-		posArray[timeCounter-1] = prePosYVal;
+	draw(context, time-1, previousYValue, time, graphPosition);
+	if(positionArray[time-1] === null){
+		positionArray[time-1] = previousYValue;
 	}
-	posArray[timeCounter] = graphPosition;
-	prePosYVal=graphPosition;
+	positionArray[time] = graphPosition;
+	previousYValue=graphPosition;
 }
 
-function drawVel(){
-	var contextVel = canvas.getContext('2d');
-	contextVel.strokeStyle="#FF6600";
-	contextVel.beginPath();
-	velArray[0]=250;
-	for(var i = 0; i < posArray.length+3; i+=4){
-		velArray[i] = (posArray[i+4]-posArray[i])+250;
+function drawVelocity(){
+	var context = canvas.getContext('2d');
+	context.strokeStyle="#FF6600";
+	context.beginPath();
+	velocityArray[0]=250;
+	for(var i = 0; i < positionArray.length+3; i+=4){
+		velocityArray[i] = positionArray[i+4]-positionArray[i]+250;
 	}
 
-	for(var j = 0; j < velArray.length+3; j+=4){
-		draw(contextVel, j, velArray[j], j+4, velArray[j+4]);
-	}
-}
-
-function drawAcc(){
-	var contextAcc = canvas.getContext('2d');
-	contextAcc.strokeStyle="#00ff00";
-	contextAcc.beginPath();
-	accArray[0]=250;
-	for(var i = 0; i < velArray.length + 3; i+=4){
-		accArray[i] = (velArray[i+4]-velArray[i])+250;
-	}
-
-	for(var j = 0; j < accArray.length+3; j+=4){
-		draw(contextAcc, j, accArray[j], j+4, accArray[j+4]);
+	for(var j = 0; j < velocityArray.length+3; j+=4){
+		draw(context, j, velocityArray[j], j+4, velocityArray[j+4]);
 	}
 }
 
-function draw(context, begX, begY, endX, endY){
-	context.moveTo(begX, begY);
+function drawAcceleration(){
+	var context = canvas.getContext('2d');
+	context.strokeStyle="#00ff00";
+	context.beginPath();
+	accelerationArray[0]=250;
+	for(var i = 0; i < velocityArray.length + 3; i+=4){
+		accelerationArray[i] = velocityArray[i+4]-velocityArray[i]+250;
+	}
+
+	for(var j = 0; j < accelerationArray.length+3; j+=4){
+		draw(context, j, accelerationArray[j], j+4, accelerationArray[j+4]);
+	}
+}
+
+function draw(context, beginX, beginY, endX, endY){
+	context.moveTo(beginX, beginY);
 	context.lineTo(endX, endY);
 	context.stroke();
 }
